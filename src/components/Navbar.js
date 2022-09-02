3a280cdcee06b29d9a48714fb11268adef488f1b
smarthood <renolddickson18@gmail.com>
2022-09-02 10:35:06 +0530

    ok

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { AppBar, Toolbar, Typography } from '@mui/material';
+
+export default function Navbar() {
+  return (
+    <AppBar>
+    <Toolbar>
+      <Typography>yoga app</Typography>
+    </Toolbar>
+    </AppBar>
+  )
+}
